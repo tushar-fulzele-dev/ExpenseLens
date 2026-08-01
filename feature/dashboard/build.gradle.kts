92ea3c844630120fdb3f467d00f375c6dfdcfe1a
tushar-fulzele-dev <129735222+tushar-fulzele-dev@users.noreply.github.com>
2026-08-01 21:09:26 +0530

    Update build.gradle.kts

--- feature/dashboard/build.gradle.kts
+++ feature/dashboard/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
 dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    ksp("com.google.dagger:hilt-compiler:2.51.1")
 }
 
 // TODO(0.2.1 / 4.1): add Compose BOM + UI dependencies once design
