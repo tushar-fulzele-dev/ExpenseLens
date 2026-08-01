4151e30f9ffeb9baa8b3d35587cca675c97a4185
tushar-fulzele-dev <129735222+tushar-fulzele-dev@users.noreply.github.com>
2026-08-01 19:52:30 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.6" apply false
 }
 
