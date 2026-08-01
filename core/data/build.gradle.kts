ed9dc62ef8cda8da3144865f6ff03403d88fbde6
tushar-fulzele-dev <129735222+tushar-fulzele-dev@users.noreply.github.com>
2026-08-01 20:05:16 +0530

    Update build.gradle.kts

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
 dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
 }
 
 // TODO(0.2.1): add Room + SQLCipher dependencies once schema (Ticket
